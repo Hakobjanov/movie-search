a0c69aaa11747cf10b31422c6e00a032e116795b
vahan akopjanian <vahan.bclub@gmail.com>
2020-05-13 18:16:49 +0300

    Updates

---
+++ precache-manifest.398b6c335b2f213340c9972b7eae7789.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e60135ed97a7b3ff7769d742ed7cf01",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "83c80cacc0d66d9e9d89",
+    "url": "/movie-search/static/css/main.fc3cdebb.chunk.css"
+  },
+  {
+    "revision": "880424f4e37b6ce8a398",
+    "url": "/movie-search/static/js/2.1081dc7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-search/static/js/2.1081dc7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c80cacc0d66d9e9d89",
+    "url": "/movie-search/static/js/main.2f4ec12f.chunk.js"
+  },
+  {
+    "revision": "deb853d98ffa0fb9c71b",
+    "url": "/movie-search/static/js/runtime-main.18c73c9f.js"
+  },
+  {
+    "revision": "a21b6d48739379522824e63d8bba4b1e",
+    "url": "/movie-search/static/media/spinner.a21b6d48.gif"
+  }
+]);
